1a3b73bde2110d477a5413d9f293b52c953a3f62
AtoKashinoki <atokashinoki@gmail.com>
2024-10-17 11:54:11 +0900

    Update Definitions.pyi

--- src/CodingTools2/Definitions.py
+++ src/CodingTools2/Definitions.py
@@ -146,7 +146,6 @@ class Msvcrt(DefinitionSkeleton):
             Del = 83
 
             """ number keys """
-            print(range(48, 10))
             n0, n1, n2, n3, n4, n5, n6, n7, n8, n9 = range(48, 48+10)
 
             """ alphabet keys """
