8cb8c1fbe602ed8fafab6df15552c431929da7e3
AtoKashinoki <atokashinoki@gmail.com>
2024-11-18 08:54:56 +0900

    Type initial commit

---
+++ src/CodingTools2/Type.py
@@ -0,0 +1,17 @@
+"""
+    CodingTools2.Type
+
+This file contain types for developing programs.
+"""
+
+
+""" Prevent execution this file """
+
+
+if __name__ == '__main__':
+    from CodingTools2.Errors import PreventExecution
+    raise PreventExecution(__file__)
+
+
+""" Types """
+
